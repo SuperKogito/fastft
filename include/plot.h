090ebd434e593bb25c202fc99cb435399b40206a
Ayoub Malek <kogito@hotmail.fr>
2024-04-23 14:54:45 +0200

    Update plot.h

--- include/plot.h
+++ include/plot.h
@@ -1,8 +1,13 @@
 #define MAX_DB_SCALE 80  
 
 #include <stdio.h>
+#include <math.h>
 
+#ifdef WIN32
+#define GNUPLOT_NAME "gnuplot.exe -persistent"
+#else
+#define GNUPLOT_NAME "gnuplot -persistent"
+#endif
 
-
-void plot_magnitude_spectrogram_in_db(float *magnitude_spectrogram, int nfft, int num_frames, int analysis_bin_limit, const char *title, int index);
+void plot_magnitude_spectrogram_in_db(float *magnitude_spectrogram, int nfft, int num_frames, int analysis_bin_limit, const char *title, int index, int use_db);
 void plot_xy(float *data, int data_count, const char *x_label, const char *y_label, const char *title);
