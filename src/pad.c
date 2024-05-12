d860d8288266c5c2e9df0757ea2b236e78e1f87c
SuperKogito <superkogito@gmail.com>
2024-05-13 01:46:11 +0200

    update and document padding code

--- src/pad.c
+++ src/pad.c
@@ -1,11 +1,21 @@
 #include "pad.h"
 
 
-void pad(float* signal, int signal_length, Padding* padding) {
+void init_padding(Padding* padding, PaddingMode mode, int width, int unpadded_signal_length, int padded_signal_length, int unpadded_num_frames, int padded_num_frames) {
+    // Initialize padding
+    padding->mode = mode;
+    padding->width = width;
+    padding->padded_signal = NULL;
+    padding->unpadded_signal_length = unpadded_signal_length;
+    padding->padded_signal_length = padded_signal_length;
+    padding->unpadded_num_frames = unpadded_num_frames;
+    padding->padded_num_frames = padded_num_frames;
+    padding->padded_signal = (float*)calloc(padded_signal_length, sizeof(float));
+}
+
+void apply_padding(float* signal, int signal_length, Padding* padding) {
     int i, left_reflected_index, right_reflected_index;
     // Calculate the left and right reflected indices based on padding mode
-    printf("%d\n", padding->mode);
-
     switch (padding->mode) {
         case REFLECT:
             // Pad the left side of the output array
@@ -35,6 +45,7 @@ void pad(float* signal, int signal_length, Padding* padding) {
             left_reflected_index  = (padding->width > 0) ? (padding->width - 1) : 0;
             right_reflected_index = (padding->width > 0) ? (signal_length - 1 - padding->width) : 0;
             break;
+            
         case EDGE:
             left_reflected_index = 0;
             right_reflected_index = (signal_length > 0) ? (signal_length - 1) : 0;
