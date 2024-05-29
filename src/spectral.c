e01c26de00dac51a1f43b157a3d435d37e1b981b
SuperKogito <superkogito@gmail.com>
2024-05-30 00:46:34 +0200

    add documentation

--- src/spectral.c
+++
@@ -1,8 +0,0 @@
-#include "spectral.h"
-
-
-void calculate_magnitude(fftwf_complex *complex_signal, float *magnitude, int length) {
-    for (int i = 0; i < length; ++i) {
-        magnitude[i] = hypotf(complex_signal[i][0], complex_signal[i][1]);
-    }
-}
--- src/window.c
+++
@@ -1,8 +0,0 @@
-#include "window.h"
-
-
-void compute_hanning_window(int n_fft, float *window) {
-    for (int i = 0; i < n_fft; i++) {
-        window[i]  = 0.5 * (1.0 - cos(2.0 * LIBROSA_PI * i / (float)(n_fft - 1)));
-    }
-}
